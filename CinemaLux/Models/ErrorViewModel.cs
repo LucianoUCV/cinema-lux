7d9f9c095339e8a91dfd5473883b748c76b3e597
Luciano <luciangligorascu@gmail.com>
2024-11-09 21:58:49 +0200

    Add project files.

---
+++ CinemaLux/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CinemaLux.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
